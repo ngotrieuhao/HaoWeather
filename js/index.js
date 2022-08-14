78519efc5799554cc020428876a729399eefeedf
LAPTOP-AD6TEQ1R\Admin <ngotrieuhao@gmail.com>
2022-08-14 14:29:21 +0700

    upgrade icon into degrees

--- js/index.js
+++ js/index.js
@@ -10,6 +10,9 @@ var time = document.querySelector(".weather__content--time");
 var content = document.querySelector(".weather__content");
 var body = document.querySelector("body");
 
+var icon = document.querySelector("#weather__icon");
+var degree = document.querySelector("#degrees");
+
 async function changeWeatherUI(capitalSearch) {
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=d5a24b803a5b4f5cd78313d2004ae267`;
   let data = await fetch(apiURL).then((res) => res.json());
@@ -27,20 +30,48 @@ async function changeWeatherUI(capitalSearch) {
 
     body.setAttribute("class", "hot");
     value.innerText = Math.round(data.main.temp - 273.15);
+    degree.innerText = "o";
+    let hasIcon = icon.classList.contains("fa-solid");
+
+    if (hasIcon || "fa-cloud-sun" || "fa-cloud" || "fa-snowflake") {
+      icon.classList.remove("fa-cloud-sun", "fa-cloud", "fa-snowflake");
+      icon.classList.add("fa-sun");
+    } else {
+      icon.classList.add("fa-solid");
+    }
 
     if (temp <= 30) {
       value.innerText = Math.round(data.main.temp - 273.15);
+      degree.innerText = "o";
       body.setAttribute("class", "warm");
+      if (hasIcon || "fa-sun" || "fa-cloud" || "fa-snowflake") {
+        icon.classList.remove("fa-sun", "fa-cloud", "fa-snowflake");
+        icon.classList.add("fa-cloud-sun");
+      } else {
+        icon.classList.add("fa-solid");
+      }
     }
     if (temp <= 22) {
       value.innerText = Math.round(data.main.temp - 273.15);
-
+      degree.innerText = "o";
       body.setAttribute("class", "cool");
+      if (hasIcon || "fa-sun" || "fa-cloud-sun" || "fa-snowflake") {
+        icon.classList.remove("fa-sun", "fa-cloud-sun", "fa-snowflake");
+        icon.classList.add("fa-cloud");
+      } else {
+        icon.classList.add("fa-solid");
+      }
     }
     if (temp <= 18) {
       value.innerText = Math.round(data.main.temp - 273.15);
-
+      degree.innerText = "o";
       body.setAttribute("class", "cold");
+      if (hasIcon || "fa-sun" || "fa-cloud-sun" || "fa-cloud") {
+        icon.classList.remove("fa-sun", "fa-cloud-sun", "fa-cloud");
+        icon.classList.add("fa-snowflake");
+      } else {
+        icon.classList.add("fa-solid");
+      }
     }
   } else {
     content.classList.add("hide");
